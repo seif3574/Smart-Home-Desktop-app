60a3d85deab09fbe149c2ae4a3d1eea48e1d8751
jana <jana.ali.shedeed.3368@gmail.com>
2026-04-27 17:04:08 +0300

    jana's task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import model.device.SmartAC;
+import model.hub.DeviceRegistry;
+import view.ViewManager;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        bootstrapDevices();
+        ViewManager.getInstance().init(primaryStage);
+        ViewManager.getInstance().showDashboard();
+    }
+
+    private static void bootstrapDevices() {
+        DeviceRegistry registry = DeviceRegistry.getInstance();
+        if (!registry.getAllDevices().isEmpty()) {
+            return;
+        }
+
+        registry.register(new SmartAC("ac1", "Living Room AC", "Living Room", "Living Room"));
+        registry.register(new SmartAC("ac2", "Bedroom AC", "Bedroom", "Bedroom"));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
